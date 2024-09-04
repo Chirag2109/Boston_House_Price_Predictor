12fa28b51117e7d6432d8644279f13a1a8fd31a4
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-09-04 23:50:31 +0530

    Update statistics.h
    removed some prototypes

--- statistics.h
+++ statistics.h
@@ -1,7 +1,4 @@
 double mean(double *arr, int n);
 double stddev(double *arr, int n, double mean);
-double r2Score(double *y_true, double *y_pred, int size);
-double meanSquaredError(double *y_true, double *y_pred, int size);
-double meanAbsoluteError(double *y_true, double *y_pred, int size);
 double covariance(double *x, double *y, int n, double meanX, double meanY);
 void correlated_matrix(double **data, double **corrMatrix, int numPoints, int numFeatures);
