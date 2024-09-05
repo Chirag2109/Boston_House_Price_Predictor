7f2739c90402cb06f395215aee89a537e4da0e57
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-09-05 22:20:35 +0530

    Update io_control.h
    not a significant change

--- io_control.h
+++ io_control.h
@@ -1,5 +1,4 @@
 void clearInputBuffer();
 void free2DArray(double **array, int rows);
-void getUserInput(double *features, int numFeatures, int *indices, int MAX_FEATURES);
-
 int readCSV(const char *filename, double ***data, int *numPoints, int *numFeatures);
+void getUserInput(double *features, int numFeatures, int *indices, int MAX_FEATURES);
