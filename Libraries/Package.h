e7b595db4c754d32147fea6ae07eeabe1156f38d
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-09-06 17:21:40 +0530

    Create Package.h

---
+++ Libraries/Package.h
@@ -0,0 +1,18 @@
+//Predefined Libraries
+#include <math.h>
+#include <ctype.h>
+#include <float.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_linalg.h>
+
+//Custom Libraries
+#include "metrics.h"
+#include "statistics.h"
+#include "io_control.h"
+#include "linear_regression.h"
+#include "Boston_Dataset_Analysis.h"
