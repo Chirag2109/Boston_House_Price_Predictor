3be42f42ba57ceef413235e1eea2f5a58a620d47
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-09-06 10:55:35 +0530

    Update statistics.h
    changed the name of a prototype

--- Libraries/statistics.h
+++ Libraries/statistics.h
@@ -1,4 +1,4 @@
 double mean(double *arr, int n);
 double stddev(double *arr, int n, double mean);
 double covariance(double *x, double *y, int n, double meanX, double meanY);
-void correlated_matrix(double **data, double **corrMatrix, int numPoints, int numFeatures);
+void correlation_matrix(double **data, double **corrMatrix, int numPoints, int numFeatures);
