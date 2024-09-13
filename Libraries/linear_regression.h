b374d7e59834e2eabb841528c055bfafa474417b
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-09-13 14:58:14 +0530

    Update linear_regression.h
    inserted include guards

--- Libraries/linear_regression.h
+++ Libraries/linear_regression.h
@@ -1,4 +1,9 @@
+#ifndef LINEAR_REGRESSION_H
+#define LINEAR_REGRESSION_H
+
 void splitData(double **data, double *labels, double ***X_train, double **y_train, double ***X_test, double **y_test, int numPoints, int numFeatures, int *correlatedFeatures, int correlatedSize, int testSize, int trainSize);
 void trainLinearRegression(double **X_train, double *y_train, double *weights, double *bias, int numPoints, int numFeatures);
 void predict(double **X_test, double *weights, double bias, double *y_pred, int numPoints, int numFeatures);
 void getCorrelatedFeatures(double **correlationMatrix, int numFeatures, int *correlatedFeatures, int *correlatedSize, double THRESHOLD);
+
+#endif
