e22d80afba23c9b69422842dec2225cecf550b8f
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-09-13 14:59:40 +0530

    Update io_control.h
    inserted include guards

--- Libraries/io_control.h
+++ Libraries/io_control.h
@@ -1,4 +1,9 @@
+#ifndef IO_CONTROL_H
+#define IO_CONTROL_H
+
 void clearInputBuffer();
 void free2DArray(double **array, int rows);
 int readCSV(const char *filename, double ***data, int *numPoints, int *numFeatures);
 void getUserInput(double *features, int numFeatures, int *indices, int MAX_FEATURES);
+
+#endif
