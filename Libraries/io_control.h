f546eed21e023a5abe261465d4d921f98b4460e9
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-09-13 16:13:47 +0530

    Update io_control.h

--- Libraries/io_control.h
+++ Libraries/io_control.h
@@ -2,6 +2,7 @@
 #define IO_CONTROL_H
 
 void clearInputBuffer();
+void validdouble(char *s, double minLimit, double maxLimit, double *VALUE);
 void free2DArray(double **array, int rows);
 int readCSV(const char *filename, double ***data, int *numPoints, int *numFeatures);
 void getUserInput(double *features, int numFeatures, int *indices, int MAX_FEATURES);
