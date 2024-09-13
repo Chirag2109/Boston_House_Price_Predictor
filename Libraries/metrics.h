fa17519cd53740b2c0d12761c28f774583aaf182
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-09-13 14:56:50 +0530

    Update metrics.h
    inserted include guards

--- Libraries/metrics.h
+++ Libraries/metrics.h
@@ -1,3 +1,8 @@
+#ifndef METRICS_H
+#define METRICS_H
+
 double r2Score(double *y_true, double *y_pred, int size);
 double meanSquaredError(double *y_true, double *y_pred, int size);
 double meanAbsoluteError(double *y_true, double *y_pred, int size);
+
+#endif
