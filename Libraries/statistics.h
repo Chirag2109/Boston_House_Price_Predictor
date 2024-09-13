5036151e33b2ebfc6fea6387fb990ffdfba2a4b1
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-09-13 14:55:36 +0530

    Update statistics.h
    inserted include guards

--- Libraries/statistics.h
+++ Libraries/statistics.h
@@ -1,4 +1,9 @@
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
 double mean(double *arr, int n);
 double stddev(double *arr, int n, double mean);
 double covariance(double *x, double *y, int n, double meanX, double meanY);
 void correlation_matrix(double **data, double **corrMatrix, int numPoints, int numFeatures);
+
+#endif
