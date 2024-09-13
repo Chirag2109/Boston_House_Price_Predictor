e188a5cdaa76e83b3fa2865d1a5e5f1bab8bf294
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-09-13 14:52:47 +0530

    Update model.h
    inserted include guards

--- Libraries/model.h
+++ Libraries/model.h
@@ -1,2 +1,7 @@
-int model(double **y_test_ptr, double **y_pred_ptr, int *test_size_ptr, int num_args, ...);
-int report(double *y_test, double *y_pred, int test_size);
+#ifndef MODEL_H
+#define MODEL_H
+
+void report(double *y_test, double *y_pred, int test_size);
+int model(double **y_test_ptr, double **y_pred_ptr, int *test_size_ptr, double THRESHOLD, double TRAIN_SIZE_RATIO);
+
+#endif
